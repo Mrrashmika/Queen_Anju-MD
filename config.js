fa1ad316477aa61c7eec9fb0f0ad492912625ec1
backalpha4 <cybersachiya70@gmail.com>
2024-09-23 14:51:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,13 +21,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "add youe session id here",
+SESSION_ID: process.env.SESSION_ID || "zRdnzKpI#E1Bd0cyLOwqP48TKTBrSBwH3Qhwc9n-7InI5iEJGaUY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/08/8e383f8918878bfc0512b2f05a81fdaa.jpeg",
 PREFIX: process.env.PREFIX || ".",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THEIR, QUEEN ANJU IS ALIVE...\n\nOWNER: JANITH RASHMIKA\n\nTHANKS FOR USING QUEEN ANJU MD\n\n> *© Qᴜᴇᴇɴ ᴀɴᴊᴜ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ*/n/n*> ɢɪᴛʜᴜʙ :* github.com/Mrrashmika/Queen_Anju-MD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-BOT_NUMBER: process.env.BOT_NUMBER || "94717775628",
+BOT_NUMBER: process.env.BOT_NUMBER || "94784608102",
 OWNER_REACT: process.env.OWNER_REACT || "🔆",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 READ_CMD: process.env.READ_CMD || "true",
